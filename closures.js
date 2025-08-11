e6d3f5a75915f75fdd3b3fe85318cbe4e9f7cd81
Atallo Takele <atallo2116@gmial.com>
2025-08-11 15:04:06 +0200

    Commit 1

---
+++ closures.js
@@ -0,0 +1,61 @@
+// function addClosure(){
+//     let x=0;
+//     return function iteration(){
+//         return x=x+1;
+//     };
+// }
+// const add=addClosure();
+// console.log(add())
+// console.log(add())
+// //or
+// console.log(addClosure()())
+
+//====== closure with setTimeout to increase an num every 2 seconds
+// function iteration(){
+//     for (var i=0; i<5; i++){
+//         setTimeout(
+//             ()=>{
+//             console.log("I: ",i)
+//         },
+//         i*1000);
+//     }
+// }
+// iteration();  //Output 4 only instead of 0, 1, 2, 3, 4
+
+//Solutions 1: use "let" or "Closures"
+// function iteration(){
+//     for(let i=0; i<5; i++){
+//         setTimeout(
+//             ()=>{
+//                 console.log("I: ", i)
+//             }, i*1000
+//         );
+//     }
+// }
+// iteration()
+
+//Solutions 2: Using closures 1 IIFC
+// function iteration() {
+//   for (var i = 0; i < 4; i++) {
+//     (function (iCopy) {
+//       setTimeout(() => {
+//         console.log("I: ", iCopy);
+//       }, i * 1000);
+//     })(i);
+//   }
+// }
+// iteration();
+
+//Solutions 3: Using Closure 2
+function iteration() {
+  
+    for (var i = 0; i < 4; i++) {
+      function addTime(iCopy) {
+        setTimeout(() => {
+          console.log("I: ", iCopy);
+        }, i * 1000);
+    }
+    addTime(i)
+    }
+}
+iteration();
---
+++ object.js
@@ -0,0 +1,24 @@
+const obj={
+    id:1,
+    name: "Abebe",
+}
+
+//===== change object to array =====
+// const arr1=[]
+// for (key in obj){
+//     arr1.push(key) //keys
+// }
+// for (key in obj){
+//     arr1.push(obj[key]) //values
+// }
+// for (key in obj){
+//     arr1.push(key, obj[key]) //values and values
+// }
+// for (key in obj){
+//     arr1.push([key, obj[key]]) //keys and values in nested array
+// }
+// console.log(arr1[0])
+// arr1=Object.values(obj)
+// arr1=Object.keys(obj)
+// arr1=Object.entries(obj)
+// console.log(arr1)
